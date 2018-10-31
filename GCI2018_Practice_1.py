576fce89bac866c73ecce8c9664db828433d30e8
hafizyunus <hafizy007@gmail.com>
2018-10-31 19:33:38 +0530

    Add practice python code

---
+++ GCI2018_Practice_1.py
@@ -0,0 +1,7 @@
+task = "GCI is great"
+n = 1
+while n <= 10:
+    print(task)
+    n += 1
+name = input("Tell me your name please ")
+print("Hello " + name + ", pleased to meet you! Did you know that your name spelt backwards is " + name[::-1])
